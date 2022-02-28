cbf4d88336ad5f45ad045db643cf78de99bbc716
Ivaylo.Ignatov <ivailo.ignatoff@gmail.com>
2022-02-28 10:18:15 +0200

    refactor gitignore

--- challenge1/main.tf
+++ challenge1/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = var.region_name
 }
 
-# resource "aws_vpc" "TerraformVPC" {
-#   cidr_block = var.vpc_cidr_range
-#   tags = {
-#     Name = var.vpc_name
-#   }
-# }
+resource "aws_vpc" "TerraformVPC" {
+  cidr_block = var.vpc_cidr_range
+  tags = {
+    Name = var.vpc_name
+  }
+}
--- challenge1/outputs.tf
+++ challenge1/outputs.tf
@@ -1,11 +1,11 @@
-# output "vpc_id" {
-#   value = aws_vpc.TerraformVPC.id
-# }
+output "vpc_id" {
+  value = aws_vpc.TerraformVPC.id
+}
 
-# output "vpc_arn" {
-#   value = aws_vpc.TerraformVPC.arn
-# }
+output "vpc_arn" {
+  value = aws_vpc.TerraformVPC.arn
+}
 
-# output "vpc_name" {
-#   value = aws_vpc.TerraformVPC.tags.Name
-# }
+output "vpc_name" {
+  value = aws_vpc.TerraformVPC.tags.Name
+}
